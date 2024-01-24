f6d463b1651656d38cac52798d71f6341f769a81
AlexMan <niallni1980@gmail.com>
2024-01-24 13:00:56 -0500

    20240124

--- AP1400-2-HW5/include/ingredient.h
+++ AP1400-2-HW5/include/ingredient.h
@@ -1,4 +1,32 @@
 #ifndef INGREDIENT_H
 #define INGREDIENT_H
 
+class Ingredient
+{
+public:
+    double get_price_unit() {
+        return this -> price_unit;
+    }
+    size_t get_units() {
+        return this -> units;
+    }
+
+    // Make it virtual to enable polymorphism
+    virtual std::string get_name() = 0;
+
+    double price() {
+        return this -> price_unit * this -> units;
+    }
+    
+
+protected:
+    Ingredient(double price_unit, size_t units);
+
+    double price_unit;
+    size_t units;
+    std::string name;
+};
+
+
 #endif // INGREDIENT_H
+
