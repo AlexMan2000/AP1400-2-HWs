192ee555b3f3b5f0954fc6785d5496f86fcc0484
AlexMan <niallni1980@gmail.com>
2024-01-24 13:36:15 -0500

    20240124

--- AP1400-2-HW5/src/espresso_based.cpp
+++ AP1400-2-HW5/src/espresso_based.cpp
@@ -2,42 +2,58 @@
 
 // Default constructor
 EspressoBased::EspressoBased() {
-
 }
 
 
 // Copy Constructor
 EspressoBased::EspressoBased(const EspressoBased& esp) {
-
+    // Valid, get_ingredients return a lvalue, which can be put in lvalue
+    this -> ingredients = esp.get_ingredients;
+    this -> name = esp.get_name();
 }
 
 
 // Destructor
 EspressoBased::~EspressoBased() {
-
+    for (const auto& i: ingredients) {
+        delete i;
+    }
+    ingredients.clear();
 }
 
 
 // Copy Assignment
 void EspressoBased::operator= (const EspressoBased& esp) {
+    if (this != &other) {
+        for (const auto& i: ingredients) {
+            delete i;
+        }
+        ingredients.clear();
+        ingredients = esp.ingredients;
+        name = esp.get_name();
 
+    }
 }
 
 
 // Member functions
 void EspressoBased::brew() {
-
+    
 }
 
 std::string EspressoBased::get_name() {
-
+    return this -> name;
 }
 
 
 double EspressoBased::price() {
+    double result;
+    for (auto& i: ingredients) {
+        result += i.price();
+    }
+    return result;
+}   
 
-}
-
-EspressoBased::get_ingredients() {
-
+std::vector<Ingredients*>& EspressoBased::get_ingredients() {
+    return this -> ingredients;
 }
